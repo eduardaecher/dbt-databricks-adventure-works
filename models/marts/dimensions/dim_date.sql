07fd879a2fd8dbe87b037693fd950f82ad144a80
Eduarda Echer <eduarda_echer@hotmail.com>
2026-01-27 19:09:54 +0000

    Ajustes para PowerBI

--- models/marts/dimensions/dim_date.sql
+++ models/marts/dimensions/dim_date.sql
@@ -3,6 +3,7 @@ with dates as (
     select distinct
         order_date
     from {{ ref('stg_adworks__salesorderheader') }}
+    where order_date is not null
 
 ),
 
@@ -10,9 +11,12 @@ date_attributes as (
 
     select
 
+
         cast(date_format(order_date, 'yyyyMMdd') as int) as date_id,
 
-        order_date as date,
+
+        cast(order_date as date) as date,
+
 
         extract(day from order_date)     as day,
         extract(month from order_date)   as month,
@@ -20,7 +24,18 @@ date_attributes as (
         extract(quarter from order_date) as quarter,
 
         date_format(order_date, 'MMMM')  as month_name,
-        date_format(order_date, 'EEEE')  as day_name
+        date_format(order_date, 'EEEE')  as day_name,
+
+
+        (extract(year from order_date) * 100 + extract(month from order_date)) 
+            as year_month_sort,
+
+
+        concat(
+            cast(extract(year from order_date) as string),
+            '-',
+            lpad(cast(extract(month from order_date) as string), 2, '0')
+        ) as year_month
 
     from dates
 
